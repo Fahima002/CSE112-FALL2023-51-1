ea4d92416cf9e8e4b1e51b5cccda14c3c8d53dcd
Zannatun Nayem Fahima <140917075+fahimagit@users.noreply.github.com>
2023-08-08 16:44:04 +0600

    Update Helpful math.cpp

--- lab 5/Helpful math.cpp
+++ lab 5/Helpful math.cpp
@@ -1,26 +1,28 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
   string str;
   cin>>str;
 
 int siz=str.size()/2+1;
-int arr[siz]
+int arr[siz];
 
 int j=0;
-for(int i=0;i<<str.size();i=i+2){
- arr[j]=str[i]-0;
-j++;
+for(int i=0;i<str.size();i=i+2){
+ arr[j]=str[i]-'0';
+  j++;
 }
 
  sort (arr,arr+siz);
 for(int i=0;i<siz;i++)
   cout<<arr[i];
-if(i!=siz-1){
-cout<<'+';
+  if(i!=siz-1){
+   cout<<'+';
    }
 }
-return 0;
+  return 0;
 }
+
 
 
